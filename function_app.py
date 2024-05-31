6677b58bdee8f11cb4e319ccb74afad4372b1f77
Sergio <sergio@rendergon.com>
2024-05-31 13:22:53 +0100

    add service bus call

--- function_app.py
+++ function_app.py
@@ -8,33 +8,38 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    print('Python HTTP trigger function processed a request.')
+    try:
 
-    max_model_tokens=5000
-    num_qa_per_section=2
-    chunk_size=1000
+        logging.info('Python HTTP trigger function processed a request.')
+        print('Python HTTP trigger function processed a request.')
 
-    examples_filename = 'examples_qa.txt'
-    json_example_filename = 'json_example_fn.txt'
+        max_model_tokens=5000
+        num_qa_per_section=2
+        chunk_size=1000
 
-    max_model_tokens=4000
-    num_qa_per_section=2
-    chunk_size=1000
+        examples_filename = 'examples_qa.txt'
+        json_example_filename = 'json_example_fn.txt'
 
-    wiki_page = req.params.get("wiki")
-    logging.info(f"User entered Wiki: {wiki_page}")
-    print(f"Received Wiki page: {wiki_page}")
+        max_model_tokens=4000
+        num_qa_per_section=2
+        chunk_size=1000
 
-    if wiki_page is None:
-        wiki_page="Roy Lichtenstein"
-        ####return func.HttpResponse(f"Could not find Wikipage for {wiki_page}. Add query param at end of url like &wiki=Roy Lichtenstein", mimetype="text/json")    
+        wiki_page = req.params.get("wiki")
+        logging.info(f"HttpTrigger: User entered Wiki: {wiki_page}")
+        print(f"HttpTrigger: Received Wiki page: {wiki_page}")
 
-    q=manage_quiz_gen.Generate_Quiz()
+        if wiki_page is None:
+            wiki_page="Roy Lichtenstein"
+            ####return func.HttpResponse(f"Could not find Wikipage for {wiki_page}. Add query param at end of url like &wiki=Roy Lichtenstein", mimetype="text/json")    
+
+        q=manage_quiz_gen.Generate_Quiz()
+        
+        response = q.quiz_manager(wiki_page, examples_filename,max_model_tokens,chunk_size,num_qa_per_section,json_example_filename)
+        return func.HttpResponse(response, mimetype="text/json")
     
-    response = q.quiz_manager(wiki_page, examples_filename,max_model_tokens,chunk_size,num_qa_per_section,json_example_filename)
-    return func.HttpResponse(response, mimetype="text/json")
-
+    except Exception as e:
+        logging.error(f"HttpTrigger: Error processing HTTP trigger: {e}")
+        return func.HttpResponse(f"HttpTrigger: Error processing request: {e}", status_code=500)
 
 
 @app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="myqueue",
