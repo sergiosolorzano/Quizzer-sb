889ba4cceccae92bfeb6a6edb7e2554d71c328c9
Sergio <sergio@rendergon.com>
2024-05-31 12:01:02 +0100

    add service bus call

--- function_app.py
+++ function_app.py
@@ -2,6 +2,7 @@ import azure.functions as func
 import logging
 import pandas as pd
 from shared_code import manage_quiz_gen
+import requests
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -39,5 +40,18 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
 @app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="myqueue",
                                connection="SERVICEBUS_CONNECTION") 
 def ServiceBusQueueTrigger(azservicebus: func.ServiceBusMessage):
+    
+    message_body = azservicebus.get_body().decode('utf-8')
+
     logging.info('Python ServiceBus Queue trigger processed a message: %s',
-                azservicebus.get_body().decode('utf-8'))
+                message_body)
+    
+    # Make HTTP request to the HTTP trigger function
+    http_trigger_url = "https://quiz-secd-funcapp.azurewebsites.net/api/http_trigger?code=yju7K8aMp--xKpfqpoIkk9qiaRbz_qb-8b8N3A4bwGMPAzFuhp8KIA%3D%3D"
+    headers = {'Content-Type': 'application/json'}
+    response = requests.get(http_trigger_url, params={"wiki": message_body}, headers=headers)
+
+    if response.status_code == 200:
+        logging.info(f"HTTP trigger response: {response.json()}")
+    else:
+        logging.error(f"Failed to trigger HTTP function. Status code: {response.status_code}, Response: {response.text}")
