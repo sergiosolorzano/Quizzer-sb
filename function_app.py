1ade7cbf49ea2e2d4d097163e8832289e5602a5d
Sergio <sergio@rendergon.com>
2024-05-31 22:12:27 +0100

    test

--- function_app.py
+++ function_app.py
@@ -17,49 +17,49 @@ async def ServiceBusQueueTrigger(azservicebus: func.ServiceBusMessage):
     logging.info('Python ServiceBus Queue trigger processed a message: %s',wiki_page)
     print('Python ServiceBus Queue trigger processed a message: %s',wiki_page)
     
-    try:
+    # try:
 
-        max_model_tokens=5000
-        num_qa_per_section=2
-        chunk_size=1000
+    #     max_model_tokens=5000
+    #     num_qa_per_section=2
+    #     chunk_size=1000
 
-        examples_filename = 'examples_qa.txt'
-        json_example_filename = 'json_example_fn.txt'
+    #     examples_filename = 'examples_qa.txt'
+    #     json_example_filename = 'json_example_fn.txt'
 
-        max_model_tokens=4000
-        num_qa_per_section=2
-        chunk_size=1000
+    #     max_model_tokens=4000
+    #     num_qa_per_section=2
+    #     chunk_size=1000
 
-        if wiki_page is None:
-            wiki_page="Roy Lichtenstein"
+    #     if wiki_page is None:
+    #         wiki_page="Roy Lichtenstein"
 
-        #create blob
-        helpF = manage_quiz_gen.HelperFunctions()
-        blob_client = helpF.BlobCreationManager()
+    #     #create blob
+    #     helpF = manage_quiz_gen.HelperFunctions()
+    #     blob_client = helpF.BlobCreationManager()
 
-        q=manage_quiz_gen.Generate_Quiz()
+    #     q=manage_quiz_gen.Generate_Quiz()
         
-        # response = await asyncio.to_thread(
-        #     q.quiz_manager, wiki_page, max_model_tokens=5000, num_qa_per_section=2, chunk_size=1000
-        # )
-        response = "hello"
+    #     # response = await asyncio.to_thread(
+    #     #     q.quiz_manager, wiki_page, max_model_tokens=5000, num_qa_per_section=2, chunk_size=1000
+    #     # )
+    #     response = "hello"
 
-        #append response
-        blob_client.append_block(
-            blob_client.append_block(response, offset=len(blob_client.download_blob().readall()), length=len(response))
-        )
+    #     #append response
+    #     blob_client.append_block(
+    #         blob_client.append_block(response, offset=len(blob_client.download_blob().readall()), length=len(response))
+    #     )
 
-        if isinstance(response, str):
-            logging.info(f"ServiceBusQueueTrigger response: {response}")
-        elif isinstance(response, dict):
-            response_text = json.dumps(response)
-            logging.info(f"ServiceBusQueueTrigger response: {response_text}")
-        else:
-            logging.error("Unexpected response type from quiz_manager")
+    #     if isinstance(response, str):
+    #         logging.info(f"ServiceBusQueueTrigger response: {response}")
+    #     elif isinstance(response, dict):
+    #         response_text = json.dumps(response)
+    #         logging.info(f"ServiceBusQueueTrigger response: {response_text}")
+    #     else:
+    #         logging.error("Unexpected response type from quiz_manager")
     
-        #ensure that your function processes each message from the Service Bus queue only once
-        await azservicebus.complete()
-        logging.info("Completed ServiceBus Queue")
+    #     #ensure that your function processes each message from the Service Bus queue only once
+    #     await azservicebus.complete()
+    #     logging.info("Completed ServiceBus Queue")
 
-    except Exception as e:
-        logging.error(f"ServiceBusQueueTrigger: Error processing wikipage: {e}")
+    # except Exception as e:
+    #     logging.error(f"ServiceBusQueueTrigger: Error processing wikipage: {e}")
